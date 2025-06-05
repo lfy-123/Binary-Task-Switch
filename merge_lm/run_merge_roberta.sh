83f20169f97b1540315f9f528bd98b0ca17668bc
Li Fangyuan <151422800+lfy-123@users.noreply.github.com>
2025-06-05 22:28:09 +0800

    Update run_merge_roberta.sh

--- merge_lm/run_merge_roberta.sh
+++ merge_lm/run_merge_roberta.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-
+source /mnt/workspace/gaojunqi/anaconda3/etc/profile.d/conda.sh
+conda activate merge_lm
+cd /mnt/workspace/gaojunqi/lifangyuan/model_merge/ours_lm/merge_lm
 
 
 python merge_roberta.py \
-    --drop_ratio 0.6 \
+    --drop_ratio 0.7 \
     --group_size 4 \
-    --batch_size 256 \
+    --batch_size 16 \
     --eval_subset_size 500 \
-    --knn_samples_per_dataset 100
-    
-    
-    
+    --knn_samples_per_dataset 100 \
+    --knn_neighbors 5
     
